7a0983b6e16438e18b9e32da2b8fff88befe5f6e
Tuan <numbthnguyen@email.arizona.edu>
2019-07-20 14:46:03 -0700

    Added About and News.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { Container, Col, Image } from 'react-bootstrap';
 import './About.css';
 
 export default class About extends Component {
     render() {
         return (
             <div>
-                ABOUT PAGE
+                <Image src="assets/zero.jpg" className="header-image" />
+                <Container>
+                    <Col xs={12} sm={8} smoffset={2}>
+                        <Image src="assets/miku.jpg" className="about-image" />
+                    </Col>
+                </Container>
             </div>
         );
     };
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import {Container, Row, Col, Image } from 'react-bootstrap';
 import './News.css';
 
 export default class News extends Component {
     render() {
         return (
             <div>
-                NEWS
+                <Image src="assets/heaven.jpg" className="header-image" />
+                <Container>
+                    <h2>News</h2>
+                    <Row>
+                        <Col xs={12} sm={8} className="main-section">
+                            <p>I like to watch anime</p>
+                            <p>Christian is a bitch</p>
+                        </Col>
+                        <Col xs={12} sm={8} className="sidebar-section">
+                            <Image src="assets/miku.jpg" />
+                            <p>Best girl</p>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         );
     };
