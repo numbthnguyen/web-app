9a5dd85ee9f1f125f62c04f29cf9c1a315c51989
Tuan <numbthnguyen@email.arizona.edu>
2019-07-20 14:29:24 -0700

    Added styles to home and Images.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import  { Link } from 'react-router-dom';
-import { Jumbotron, Container, Button } from 'react-bootstrap';
+import { Jumbotron, Container, Button, Row, Col, Image } from 'react-bootstrap';
 import './Home.css';
 
 export default class Home extends Component {
@@ -10,10 +10,27 @@ export default class Home extends Component {
                 <Jumbotron>
                     <h2>Welcome to anime</h2>
                     <p>Are a weeb?</p>
+                    <Link to="/about">
+                        <Button bsstyle="primary">About</Button>
+                    </Link>
                 </Jumbotron>
-                <Link to="/about">
-                    <Button bsstyle="primary">About</Button>
-                </Link>
+                <Row className="show-grid text-center">
+                    <Col xs={12} sm={4} className="person-wrapper">
+                        <Image src="assets/heaven.jpg" className="profile-pic" />
+                        <h3>Heaven's Feel</h3>
+                        <p>Good movie</p>
+                    </Col>
+                    <Col xs={12} sm={4} className="person-wrapper">
+                        <Image src="assets/miku.jpg" circle="true" className="profile-pic" />
+                        <h3>Miku</h3>
+                        <p>Good movie</p>
+                    </Col>
+                    <Col xs={12} sm={4} className="person-wrapper">
+                        <Image src="assets/zero.jpg" className="profile-pic" />
+                        <h3>Fate zero</h3>
+                        <p>Good movie</p>
+                    </Col>
+                </Row>
             </Container>
         );
     };
